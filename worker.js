74e5ab9e43e7077ba757c40db3d40ad44e501c6c
Naushikha <naushikha.jayawickrama@gmail.com>
2021-05-12 06:40:56 +0530

    Added authorization functionality

--- worker.js
+++ worker.js
@@ -1,11 +1,21 @@
-var hostname = "https://spotify.naush.workers.dev/";
+// ! CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN are environment variables defined in Cloudflare
 
 addEventListener("fetch", (event) => {
+  var url = new URL(event.request.url);
+
   // Simple routing
-  var url = event.request.url.replace(hostname, "").replaceAll("/", "");
-  switch (url) {
+  var route = url.pathname.replaceAll("/", "");
+  switch (route) {
     case "hello":
       event.respondWith(handleHello(event.request));
+
+      break;
+    case "authorize":
+      event.respondWith(handleAuthorization(event.request));
+
+      break;
+    case "callback":
+      event.respondWith(handleCallback(event.request));
 
       break;
     case "get-now-playing":
@@ -18,20 +28,58 @@ addEventListener("fetch", (event) => {
 });
 
 async function handleHello(request) {
-  const data = {
-    hello: "This is my first middleware thingy!",
+  return new Response("Hello!, This is my first middleware thingy.");
+}
+
+async function handleAuthorization(request) {
+  var state = "SPOTIFY_WIDGET"; // No use? remove?
+  var url = new URL(request.url);
+  var callback_url = `${url.protocol}//${url.hostname}/callback`;
+  var scope = "user-read-private user-read-email";
+  var params = {
+    response_type: "code",
+    client_id: CLIENT_ID,
+    scope: scope,
+    redirect_uri: callback_url,
+    state: state,
   };
-  const json = JSON.stringify(data, null, 2);
-  return new Response(json, {
-    headers: {
-      "content-type": "application/json;charset=UTF-8",
-    },
-  });
+  var queryString = Object.keys(params)
+    .map((key) => key + "=" + params[key])
+    .join("&"); // https://howchoo.com/javascript/how-to-turn-an-object-into-query-string-parameters-in-javascript
+  return Response.redirect(
+    "https://accounts.spotify.com/authorize?" + queryString,
+    302
+  );
+}
+
+async function handleCallback(request) {
+  var url = new URL(request.url);
+  var callback_url = `${url.protocol}//${url.hostname}/callback`;
+  var code = url.searchParams.get("code") || null;
+  var state = url.searchParams.get("state") || null; // Might remove later? no use
+
+  if (state === "SPOTIFY_WIDGET") {
+    return fetch("https://accounts.spotify.com/api/token", {
+      method: "post",
+      headers: {
+        Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `code=${code}&redirect_uri=${callback_url}&grant_type=authorization_code`,
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else
+          return new Response("Something went wrong, retry authorization :/");
+      })
+      .then((data) => {
+        return new Response(`Your refresh token is: ${data.refresh_token}`);
+      });
+  } else return new Response("Something went wrong, retry authorization :/");
 }
 
 async function handleNowPlaying(request) {
-  const refresh_token = await SPOTIFY_KV.get("REFRESH_TOKEN");
-
   // Get a new access token everytime :D << Easy way out (Without checking expire time)
   let response = await fetch("https://accounts.spotify.com/api/token", {
     method: "post",
@@ -39,7 +87,7 @@ async function handleNowPlaying(request) {
       Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
       "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: `grant_type=refresh_token&refresh_token=${refresh_token}`,
+    body: `grant_type=refresh_token&refresh_token=${REFRESH_TOKEN}`,
   });
   // Assuming no errors would occur :P
   response = await response.json();
